efd9c7dc777475bfb2af61914437c0f7984e85fb
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-06-02 04:49:24 -0300

    Primeira estrategia

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let reiniciar = document.querySelector("#btn-reiniciar");
 
 /*COLOCANDO ELEMENTOS NA TELA*/
 let vez = true
-
+console.log(posicao[0], posicao[1])
 posicao.forEach( ()=>{
     tabuleiro.addEventListener("click", function(event){
         if(event.target.tagName == "SPAN"){
@@ -23,6 +23,15 @@ posicao.forEach( ()=>{
                 vez = true
             }
         }
-
+        estrategia1()
     })
 })
+
+
+/*ESTRATEGIAS*/
+function estrategia1(){
+
+    if(posicao[0].textContent === posicao[1].textContent && posicao[1].textContent === posicao[2].textContent){
+        console.log("venceu")
+    }
+}
