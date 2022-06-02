6046c6badb21f5da45ae55c0ad6b2e12e7e24381
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-06-02 18:05:58 -0300

    Criando jogadas e estratégias para vencer o jogo entre duas pessoas

--- main.js
+++ main.js
@@ -16,12 +16,14 @@ let vez = true
         if(event.target.tagName == "SPAN"){
             if(vez){
                 event.target.textContent = "X"
-                vez = false
                 validarJogada(event.target.textContent);
+                vez = false
+                
             }else{
                 event.target.textContent = "0"
-                vez = true
                 validarJogada(event.target.textContent);
+                vez = true
+                
             }
         }
     })
@@ -29,20 +31,86 @@ let vez = true
 
 /*ESTRATEGIAS*/
 function validarJogada(jogada){
-    if(estrategia1(jogada)){
-        console.log("jogador "+ jogada +" venceu")
+    if(
+        estrategia1(jogada)|| 
+        estrategia2(jogada)|| 
+        estrategia3(jogada)||
+        estrategia4(jogada)||
+        estrategia5(jogada)||
+        estrategia6(jogada)||
+        estrategia7(jogada)||
+        estrategia8(jogada)
+        ){
+        status.textContent = "vitória do jogador " + jogada
+        if(jogada == "0"){
+            pontos0.textContent = Number(pontos0.textContent) + 1
+            setTimeout( () => {
+                resetarJogo()
+            },1000)
+            
+        }
+        if(jogada == "X"){
+            pontosX.textContent = Number(pontosX.textContent) + 1
+            setTimeout( () => {
+                resetarJogo()
+            },1000)
+        }
     }else{
 
     }
 }
+
 function estrategia1(valor){
     let resultado = (posicao[0].textContent == valor && posicao[1].textContent == valor && posicao[2].textContent == valor)
     return resultado
 }
+function estrategia2(valor){
+    let resultado = (posicao[3].textContent == valor && posicao[4].textContent == valor && posicao[5].textContent == valor)
+    return resultado
+}
+function estrategia3(valor){
+    let resultado = (posicao[6].textContent == valor && posicao[7].textContent == valor && posicao[8].textContent == valor)
+    return resultado
+}
+function estrategia4(valor){
+    let resultado = (posicao[0].textContent == valor && posicao[3].textContent == valor && posicao[6].textContent == valor)
+    return resultado
+}
+function estrategia5(valor){
+    let resultado = (posicao[1].textContent == valor && posicao[4].textContent == valor && posicao[7].textContent == valor)
+    return resultado
+}
+function estrategia6(valor){
+    let resultado = (posicao[2].textContent == valor && posicao[5].textContent == valor && posicao[8].textContent == valor)
+    return resultado
+}
+function estrategia7(valor){
+    let resultado = (posicao[0].textContent == valor && posicao[4].textContent == valor && posicao[8].textContent == valor)
+    return resultado
+}
+function estrategia8(valor){
+    let resultado = (posicao[2].textContent == valor && posicao[4].textContent == valor && posicao[6].textContent == valor)
+    return resultado
+}
+
+
 
 /*REINICIAR*/
-reiniciar.addEventListener("click", function(){
+
+function resetarJogo(){
     for(var i = 0; i < 9; i++){
         posicao[i].textContent = ""
+        vez = true
+        status.textContent = "começar"
+    }
+}
+
+reiniciar.addEventListener("click", function reiniciarJogo(){
+    for(var i = 0; i < 9; i++){
+        posicao[i].textContent = ""
+        vez = true
+        status.textContent = "começar"
+        pontos0.textContent = "0"
+        pontosX.textContent = "0"
     }
 })
